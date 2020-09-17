ac74e710892a86c5e35892f000e5bf0f340a011b
Anastasia Hristian <an.hristian@gmail.com>
2020-09-16 23:45:43 -0600

    practicing.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,12 +2,21 @@ package academy.learnprogramming;
 
 public class Main {
 
+  /**
+   *
+   * @param args command line arguments.
+   */
   public static void main(String[] args) {
+    //Printing size of arguments.
     System.out.println("args-size= " + args.length);
-
+  /*
+  printing arguments
+  another line
+   */
     for (int i = 0; i < args.length; i++){
       System.out.println("args[" + i + "]=" + args[i]);
     }
   }
+
 
 }
