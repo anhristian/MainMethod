620171fe336c4fe4f16b61b93b3fa865c587bd7b
Anastasia Hristian <an.hristian@gmail.com>
2020-09-17 21:04:42 -0600

    practicing.

---
+++ src/academy/learnprogramming/ConflictExample.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import java.util.*;
+import  java.sql.*;
+import java.util.Date;
+
+public class ConflictExample {
+
+  public static void main(String[] args) {
+    Date data;
+    java.sql.Date sql;
+  }
+
+}
